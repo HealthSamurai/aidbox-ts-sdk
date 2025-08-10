ebacfa7582cadfa579df4bcfef3987bc12c9e2b4
Marat Surmashev <muradbei@gmail.com>
2025-08-10 12:52:35 +0200

    Add storycode addon

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -4,7 +4,9 @@ const config: StorybookConfig = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [],
+  "addons": [
+    "@storybook/addon-storysource"
+  ],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
