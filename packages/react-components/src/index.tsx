bac44aa12b9ee64fbabf5e92f4e32d8e6c0eaa24
Panthevm <panthevm@yandex.ru>
2025-09-01 14:35:11 +0300

    Add Play icon

--- packages/react-components/src/index.tsx
+++ packages/react-components/src/index.tsx
@@ -3,6 +3,7 @@
 export * from "./components/code-editor";
 export * from "./components/copy-icon";
 export * from "./components/request-line-editor";
+export * from "./icons";
 export * from "./shadcn/components/ui/accordion";
 export * from "./shadcn/components/ui/alert";
 export * from "./shadcn/components/ui/alert-dialog";
