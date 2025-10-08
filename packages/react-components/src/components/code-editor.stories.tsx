caa08efe281de78e996a295708b1d4f5aa64e855
Vasilii Kupriakov <vasiliy.kupryakov@health-samurai.io>
2025-10-08 23:05:03 +0800

    WIP: implement better codemirror sync with native inputs

--- packages/react-components/src/components/code-editor.stories.tsx
+++ packages/react-components/src/components/code-editor.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 import React from "react";
 import { Input } from "#shadcn/components/ui/input.js";
 import { CodeEditor } from "./code-editor";
+import * as http from "./code-editor/http";
 
 const meta: Meta<typeof CodeEditor> = {
 	title: "Component/Editor",
@@ -36,168 +37,6 @@ export const Default: Story = {
 	),
 };
 
-type ParsedHeader = {
-	name: string;
-	nameTrivia: string;
-	value: string;
-	valueTrivia: string;
-};
-
-type Parsed = {
-	method: string;
-	methodTrivia: string;
-	path: string;
-	pathTrivia: string;
-	headers: ParsedHeader[];
-	headersTrivia: string;
-};
-
-function parse(query: string): Parsed {
-	let hi = 0;
-	let lo = 0;
-	const res: Parsed = {
-		method: "",
-		methodTrivia: "",
-		path: "",
-		pathTrivia: "",
-		headers: [],
-		headersTrivia: "",
-	};
-
-	// Note that we iterate by code units, but it doesn't change correctness.
-	// method
-	for (hi = 0; hi < query.length; ++hi) {
-		const c = query[hi];
-		if (c === " " || c === "\t" || c === "\n") {
-			break;
-		}
-	}
-	res.method = query.substring(lo, hi);
-	lo = hi;
-	if (lo >= query.length) {
-		return res;
-	}
-
-	// method trivia
-	for (hi = lo; hi < query.length; ++hi) {
-		const c = query[hi];
-		if (!(c === " " || c === "\t")) {
-			break;
-		}
-	}
-	res.methodTrivia = query.substring(lo, hi);
-	lo = hi;
-	if (lo >= query.length) {
-		return res;
-	}
-
-	// path
-	for (hi = lo; hi < query.length; ++hi) {
-		const c = query[hi];
-		if (c === "\n") {
-			break;
-		}
-	}
-	res.path = query.substring(lo, hi);
-	lo = hi;
-	if (lo >= query.length) {
-		return res;
-	}
-
-	// path trivia
-	if (query[hi] === "\n") {
-		hi += 1;
-		res.pathTrivia = query.substring(lo, hi);
-	}
-	lo = hi;
-	if (lo >= query.length) {
-		return res;
-	}
-
-	// headers
-	let header: ParsedHeader = {
-		name: "",
-		nameTrivia: "",
-		value: "",
-		valueTrivia: "",
-	};
-	let headerReady = false;
-	// SAFETY: don't decrease hi inside this loop.
-	for (hi = lo; hi < query.length; ++hi) {
-		if (headerReady) {
-			res.headers.push(header);
-		}
-		header = { name: "", nameTrivia: "", value: "", valueTrivia: "" };
-		headerReady = false;
-
-		if (query[hi] === "\n") {
-			// end of headers
-			break;
-		}
-
-		// header name
-		for (lo = hi; hi <= query.length; ++hi) {
-			const c = query[hi];
-			if (c === " " || c === "\t" || c === "\n" || c === ":") {
-				break;
-			}
-		}
-		headerReady = true;
-		header.name = query.substring(lo, hi);
-		lo = hi;
-		if (lo >= query.length) {
-			break;
-		}
-
-		// header name trivia
-		let colonFound = false;
-		for (lo = hi; hi <= query.length; ++hi) {
-			const c = query[hi];
-			if (c === ":" && !colonFound) {
-				colonFound = true;
-			} else if (!(c === " " || c === "\t")) {
-				break;
-			}
-		}
-		header.nameTrivia = query.substring(lo, hi);
-		lo = hi;
-		if (lo >= query.length) {
-			break;
-		}
-
-		// header value
-		for (hi = lo; hi < query.length; ++hi) {
-			const c = query[hi];
-			if (c === "\n") {
-				break;
-			}
-		}
-		header.value = query.substring(lo, hi);
-		lo = hi;
-		if (lo >= query.length) {
-			break;
-		}
-
-		// header value trivia
-		if (query[hi] === "\n") {
-			header.valueTrivia = query.substring(lo, hi + 1);
-		}
-		lo = hi + 1;
-		if (lo >= query.length) {
-			break;
-		}
-	}
-	if (headerReady) {
-		res.headers.push(header);
-	}
-
-	if (query[hi] === "\n") {
-		res.headersTrivia = query.substring(lo, hi + 1);
-	}
-
-	return res;
-}
-
 const MethodInput = React.memo(function MethodInput({
 	method,
 	onMethodChange,
@@ -227,7 +66,7 @@ const PathInput = React.memo(function PathInput({
 }) {
 	return (
 		<div className="flex justify-center items-baseline">
-			{"Path: "}
+			Path:
 			<Input
 				type="text"
 				className="inline"
@@ -239,14 +78,8 @@ const PathInput = React.memo(function PathInput({
 });
 
 function ComplexComp() {
-	const [rawQuery, setRawQuery] = React.useState("");
-	const parsed = React.useMemo(() => parse(rawQuery), [rawQuery]);
-	const parsedRef = React.useRef(parsed);
-	React.useEffect(() => {
-		parsedRef.current = parsed;
-	}, [parsed]);
-	const method = React.useMemo(() => parsed.method, [parsed.method]);
-	const path = React.useMemo(() => parsed.path, [parsed.path]);
+	const [path, setPath] = React.useState("");
+	const [method, setMethod] = React.useState("");
 
 	const viewRef = React.useRef<EditorView | null>(null);
 
@@ -263,16 +96,8 @@ function ComplexComp() {
 				return null;
 			}
 
-			const from = 0;
-			const to = parsedRef.current.method.length;
-
-			view.dispatch({
-				changes: {
-					from: from,
-					to: to,
-					insert: newVal,
-				},
-			});
+			http.setMethod(view, newVal);
+			setMethod(newVal);
 		},
 		[],
 	);
@@ -286,18 +111,8 @@ function ComplexComp() {
 				return null;
 			}
 
-			const from =
-				parsedRef.current.method.length + parsedRef.current.methodTrivia.length;
-			const to = from + parsedRef.current.path.length;
-			console.log(from, to);
-
-			view.dispatch({
-				changes: {
-					from: from,
-					to: to,
-					insert: newVal,
-				},
-			});
+			http.setPath(view, newVal);
+			setPath(newVal);
 		},
 		[],
 	);
@@ -310,9 +125,11 @@ function ComplexComp() {
 				<CodeEditor
 					mode="http"
 					onUpdate={(update) => {
-						if (update.docChanged) {
-							setRawQuery(update.state.doc.toString());
-						}
+						const method = http.getMethod(update.view);
+						const path = http.getPath(update.view);
+
+						setMethod(method);
+						setPath(path);
 					}}
 					viewCallback={(view) => {
 						viewRef.current = view;
