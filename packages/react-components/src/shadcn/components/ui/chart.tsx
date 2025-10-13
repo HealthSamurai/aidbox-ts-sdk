1fba6a34432e8a72e18b4ae0e3ac59f0cab741d8
Vasilii Kupriakov <vasiliy.kupryakov@health-samurai.io>
2025-10-13 21:25:17 +0800

    Convert all top-level fns to named functions

--- packages/react-components/src/shadcn/components/ui/chart.tsx
+++ packages/react-components/src/shadcn/components/ui/chart.tsx
@@ -68,7 +68,7 @@ function ChartContainer({
 	);
 }
 
-const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
+function ChartStyle({ id, config }: { id: string; config: ChartConfig }) {
 	const colorConfig = Object.entries(config).filter(
 		([, config]) => config.theme || config.color,
 	);
@@ -100,7 +100,7 @@ ${colorConfig
 			}}
 		/>
 	);
-};
+}
 
 const ChartTooltip = RechartsPrimitive.Tooltip;
 
