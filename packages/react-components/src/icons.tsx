296a2ef1b68b55556dbe5591ffdbc7f0bfa93d1a
Panthevm <panthevm@yandex.ru>
2026-02-11 17:12:32 +0300

    Add error line gutter markers and autocomplete icons to CodeEditor
    - Add issueLineNumbers prop that highlights gutter line numbers in error color
      using CodeMirror StateField + GutterMarker
    - Add custom autocomplete rendering: type-based icons (function, complex type,
      primitive) and hover tooltip for completion detail
    - Add SquareFunctionIcon to icons
    - Increase autocomplete dropdown max height

--- packages/react-components/src/icons.tsx
+++ packages/react-components/src/icons.tsx
@@ -116,6 +116,28 @@ export function BackboneElementIcon() {
 	);
 }
 
+export function SquareFunctionIcon() {
+	return (
+		<svg
+			width="16"
+			height="16"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="#717684"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			xmlns="http://www.w3.org/2000/svg"
+			aria-label="Function"
+			role="img"
+		>
+			<rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+			<path d="M9 17c2 0 2.8-1 2.8-2.8V10c0-2 1-3.3 3.2-3" />
+			<path d="M9 11.2h5.7" />
+		</svg>
+	);
+}
+
 export function TypCodeIcon() {
 	return (
 		<svg
