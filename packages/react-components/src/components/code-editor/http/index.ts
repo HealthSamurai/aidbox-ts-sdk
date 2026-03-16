d4808f87f1de455db68e45066b739acd9c475a23
Panthevm <panthevm@yandex.ru>
2026-03-16 17:47:21 +0300

    Add HTTP method syntax highlighting to code editor
    Color GET/POST/PUT/PATCH/DELETE in raw HTTP editor using
    utility color tokens matching the request method selector.

--- packages/react-components/src/components/code-editor/http/index.ts
+++ packages/react-components/src/components/code-editor/http/index.ts
@@ -4,11 +4,21 @@ import {
 	type Language,
 	LanguageSupport,
 	LRLanguage,
+	syntaxTree,
 } from "@codemirror/language";
+import { RangeSetBuilder } from "@codemirror/state";
+import {
+	Decoration,
+	type DecorationSet,
+	EditorView,
+	ViewPlugin,
+	type ViewUpdate,
+} from "@codemirror/view";
 import { parseMixed } from "@lezer/common";
 import { styleTags, tags } from "@lezer/highlight";
 import type { LRParser } from "@lezer/lr";
 import { parser } from "./grammar/http";
+import { HttpRequestMethod } from "./grammar/http.terms";
 
 function makeParser(
 	bodyLanguages: (contentType: string) => Language | null,
@@ -78,10 +88,71 @@ function makeParser(
 	});
 }
 
+const methodDecorations: Record<string, Decoration> = {
+	GET: Decoration.mark({ class: "cm-http-method-get" }),
+	POST: Decoration.mark({ class: "cm-http-method-post" }),
+	PUT: Decoration.mark({ class: "cm-http-method-put" }),
+	PATCH: Decoration.mark({ class: "cm-http-method-patch" }),
+	DELETE: Decoration.mark({ class: "cm-http-method-delete" }),
+};
+
+function buildMethodDecorations(view: EditorView): DecorationSet {
+	const builder = new RangeSetBuilder<Decoration>();
+	const tree = syntaxTree(view.state);
+	tree.iterate({
+		enter(node) {
+			if (node.type.id === HttpRequestMethod) {
+				const text = view.state.sliceDoc(node.from, node.to).toUpperCase();
+				const deco = methodDecorations[text];
+				if (deco) {
+					builder.add(node.from, node.to, deco);
+				}
+			}
+		},
+	});
+	return builder.finish();
+}
+
+const httpMethodHighlighter = ViewPlugin.fromClass(
+	class {
+		decorations: DecorationSet;
+		constructor(view: EditorView) {
+			this.decorations = buildMethodDecorations(view);
+		}
+		update(update: ViewUpdate) {
+			if (update.docChanged || update.viewportChanged) {
+				this.decorations = buildMethodDecorations(update.view);
+			}
+		}
+	},
+	{ decorations: (v) => v.decorations },
+);
+
+const httpMethodTheme = EditorView.baseTheme({
+	".cm-http-method-get": {
+		color: "var(--color-utility-green)",
+	},
+	".cm-http-method-post": {
+		color: "var(--color-utility-yellow)",
+	},
+	".cm-http-method-put": {
+		color: "var(--color-utility-blue)",
+	},
+	".cm-http-method-patch": {
+		color: "var(--color-utility-violet)",
+	},
+	".cm-http-method-delete": {
+		color: "var(--color-utility-red)",
+	},
+});
+
 function http(bodyLanguages: (contentType: string) => Language | null) {
 	const parser = makeParser(bodyLanguages);
 	const language = LRLanguage.define({ parser: parser });
-	return new LanguageSupport(language, []);
+	return new LanguageSupport(language, [
+		httpMethodHighlighter,
+		httpMethodTheme,
+	]);
 }
 
 export { http };
