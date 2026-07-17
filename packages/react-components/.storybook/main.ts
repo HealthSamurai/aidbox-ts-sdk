d1b684e0df3ee79db02eb447a0c7dad31b0a3c36
Andrey Listopadov <andreyorst@gmail.com>
2026-07-17 15:07:15 +0300

    fix storybook

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -1,11 +1,17 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: ["@storybook/addon-docs"],
-	framework: "@storybook/react-vite",
+	addons: [getAbsolutePath("@storybook/addon-docs")],
+	framework: getAbsolutePath("@storybook/react-vite"),
 	typescript: {
 		reactDocgen: "react-docgen-typescript",
 	},
 };
 export default config;
+
+function getAbsolutePath(value: string): string {
+	return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
